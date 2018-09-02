38efd190d1afcc0186c651d8125a0a04900da825
Lean <leanmkrasmussen@gmail.com>
2018-09-02 16:05:10 +0200

    Added advanced scroll with logic

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-//module
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+    let scropp = wScroll*-0.15;
+    let textscroll =  wScroll*1.5;
+    console.log(scropp +" scroop " + wScroll +" window scroll ")
+    $('.parallax-advanced__background--front').css({
+        "transform":"translate(0px," + scropp +"px)"
+    })
+    $('.parallax-advanced__background--text').css({
+        "transform":"translate(0px," + textscroll +"px)",
+        "opacity": 1- (textscroll*0.002),
+    })
+})
 
+//module
 var TMbling= angular.module('TMbling',['ngRoute']);
 
 //controller
 
-TMbling.controller('navController',['$scope'],function($scope){
-
-})
+TMbling.controller('navController',['$scope',function($scope){
+    console.log($scope)
+}])
 
 //routes
 TMbling.config(function($routeProvider){
 
     $routeProvider
 
-    .when('/',{
-        templateURL: 'pages/home.htm',
+    //route for home page
+    .when('/', {
+        templateUrl: 'pages/home.html',
         controller:'homeController'
     })
-    .when('/parallax',{
-        templateURL: 'pages/parallax.htm',
+
+    .when('/parallax', {
+        templateUrl: 'pages/parallaxbasic.html',
         controller: 'parallaxController'
     })
 
-})
+    .when('/advancedParallax', {
+        templateUrl: 'pages/advancedParallax.html',
+        controller: 'advancedParallaxController'
+    })
+    .when('/SVG', {
+        templateUrl: 'pages/svg.html',
+        controller: 'SVGController'
+    })
+
+});
